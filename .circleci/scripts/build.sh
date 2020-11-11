07328203d7c20b7c56f1d95d76e992b308236906
Joscelyn James <josce.james7@hotmail.com>
2020-11-11 16:33:56 -0700

    added env to docker

--- .circleci/scripts/build.sh
+++ .circleci/scripts/build.sh
@@ -3,4 +3,10 @@
 REMOTE=413774799288.dkr.ecr.us-west-2.amazonaws.com
 REPO=mui-theme-server
 
-docker build -f Dockerfile -t $REMOTE/$REPO:latest .
+docker build \
+ --build-arg MUI_THEME_ENV_API_DB_HOST=$MUI_THEME_ENV_API_DB_HOST \
+ --build-arg MUI_THEME_ENV_API_DB_PORT=$MUI_THEME_ENV_API_DB_PORT \
+ --build-arg MUI_THEME_ENV_API_DB_NAME=$MUI_THEME_ENV_API_DB_NAME \
+ --build-arg MUI_THEME_ENV_API_DB_USER=$MUI_THEME_ENV_API_DB_USER \
+ --build-arg MUI_THEME_ENV_API_SSL_MODE=$MUI_THEME_ENV_API_SSL_MODE \
+  -f Dockerfile -t $REMOTE/$REPO:latest .
